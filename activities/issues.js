d83991f74824043be1077114f306c9bb2e3451a2
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-02-19 16:22:45 +0100

    Changed indentation from 4 spaces to 2.

--- activities/issues.js
+++ activities/issues.js
@@ -2,23 +2,23 @@ const api = require('./common/api');
 
 module.exports = async function (activity) {
 
-    try {
+  try {
 
-        api.initialize(activity);
+    api.initialize(activity);
 
-        const response = await api('/issues?state=opened&scope=assigned_to_me');
+    const response = await api('/issues?state=opened&scope=assigned_to_me');
 
-        // convert response to items[]
-        activity.Response.Data = api.convertIssues(response);
-    } catch (error) {
+    // convert response to items[]
+    activity.Response.Data = api.convertIssues(response);
+  } catch (error) {
 
-        // return error response
-        var m = error.message;
-        if (error.stack) m = m + ": " + error.stack;
+    // return error response
+    var m = error.message;
+    if (error.stack) m = m + ": " + error.stack;
 
-        activity.Response.ErrorCode = (error.response && error.response.statusCode) || 500;
-        activity.Response.Data = { ErrorText: m };
+    activity.Response.ErrorCode = (error.response && error.response.statusCode) || 500;
+    activity.Response.Data = { ErrorText: m };
 
-    }
+  }
 
 };
--- activities/myticket-status.js
+++ activities/myticket-status.js
@@ -5,39 +5,39 @@ const handleError = require('@adenin/cf-activity').handleError;
 const api = require('./common/api');
 
 module.exports = async (activity) => {
-    try {
-        api.initialize(activity);
+  try {
+    api.initialize(activity);
 
-        const userNameResponse = await api('/user');
+    const userNameResponse = await api('/user');
 
-        let username = userNameResponse.body.username;
-        let openIssuesUrl = `https://gitlab.com/dashboard/issues?assignee_username=${username}`;
+    let username = userNameResponse.body.username;
+    let openIssuesUrl = `https://gitlab.com/dashboard/issues?assignee_username=${username}`;
 
-        const response = await api('/issues?state=opened&scope=assigned_to_me');
+    const response = await api('/issues?state=opened&scope=assigned_to_me');
 
-        let ticketStatus = {
-            title: 'Open Tickets',
-            url: openIssuesUrl,
-            urlLabel: 'All tickets',
-        };
+    let ticketStatus = {
+      title: 'Open Tickets',
+      url: openIssuesUrl,
+      urlLabel: 'All tickets',
+    };
 
-        if (response.body.length != 0) {
-            ticketStatus = {
-                description: `You have ${response.body.length} tickets assigned`,
-                color: 'blue',
-                value: response.body.length,
-                actionable: true
-            }
-        } else {
-            ticketStatus = {
-                description: `You have no tickets assigned`,
-                actionable: false
-            }
-        }
-
-        activity.Response.Data = ticketStatus;
-
-    } catch (error) {
-        handleError(error, activity);
+    if (response.body.length != 0) {
+      ticketStatus = {
+        description: `You have ${response.body.length} tickets assigned`,
+        color: 'blue',
+        value: response.body.length,
+        actionable: true
+      }
+    } else {
+      ticketStatus = {
+        description: `You have no tickets assigned`,
+        actionable: false
+      }
     }
+
+    activity.Response.Data = ticketStatus;
+
+  } catch (error) {
+    handleError(error, activity);
+  }
 };
