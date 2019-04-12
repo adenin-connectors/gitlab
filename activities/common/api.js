e265de7e492ab34feff9d228efb5773aaf22f100
olliejm <olliejm@protonmail.com>
2019-04-12 11:13:25 +0100

    prepare for azure pipeline

--- activities/common/api.js
+++ activities/common/api.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const got = require('got');
 const HttpAgent = require('agentkeepalive');
 const HttpsAgent = HttpAgent.HttpsAgent;
@@ -23,34 +24,41 @@ function api(path, opts) {
     'user-agent': 'adenin Now Assistant Connector, https://www.adenin.com/now-assistant'
   }, opts.headers);
 
-  if (opts.token) {
-    opts.headers.Authorization = `Bearer ${opts.token}`;
-  }
+  if (opts.token) opts.headers.Authorization = `Bearer ${opts.token}`;
 
   const url = /^http(s)\:\/\/?/.test(path) && opts.endpoint ? path : opts.endpoint + path;
 
-  if (opts.stream) {
-    return got.stream(url, opts);
-  }
+  if (opts.stream) return got.stream(url, opts);
 
-  return got(url, opts).catch(err => {
+  return got(url, opts).catch((err) => {
     throw err;
   });
 }
+
 //**maps response data to items */
 api.convertIssues = function (response) {
-  let items = [];
-  let body = response.body;
+  const items = [];
+  const body = response.body;
 
   for (let i = 0; i < body.length; i++) {
-    let raw = body[i];
-    let item = { id: raw.id, title: raw.title, description: raw.description, link: raw.web_url, raw: raw }
+    const raw = body[i];
+
+    const item = {
+      id: raw.id,
+      title: raw.title,
+      description: raw.description,
+      link: raw.web_url,
+      raw: raw
+    };
 
     items.push(item);
   }
 
-  return { items: items };
-}
+  return {
+    items: items
+  };
+};
+
 const helpers = [
   'get',
   'post',
@@ -60,15 +68,15 @@ const helpers = [
   'delete'
 ];
 
-api.stream = (url, opts) => apigot(url, Object.assign({}, opts, {
+api.stream = (url, opts) => got(url, Object.assign({}, opts, {
   json: false,
   stream: true
 }));
 
 for (const x of helpers) {
   const method = x.toUpperCase();
-  api[x] = (url, opts) => api(url, Object.assign({}, opts, { method }));
-  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, { method }));
+  api[x] = (url, opts) => api(url, Object.assign({}, opts, {method}));
+  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, {method}));
 }
 
 module.exports = api;
