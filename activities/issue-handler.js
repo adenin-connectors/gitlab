ce53acc2094e08bdb838d8a6b5556416bac5c3c7
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-05-14 14:59:20 +0200

    Updated 'issue-handler'.

--- activities/issue-handler.js
+++ activities/issue-handler.js
@@ -1,10 +1,11 @@
 'use strict';
+const api = require('./common/api');
 
 module.exports = async (activity) => {
   try {
 
     // validate X-Gitlab-Token header
-    if (!activity.Context.connector.custom1 ||(activity.Context.connector.custom1 != activity.Request.Headers["X-Gitlab-Token"])) {
+    if (!activity.Context.connector.custom1 || (activity.Context.connector.custom1 != activity.Request.Headers["X-Gitlab-Token"])) {
       activity.Response.ErrorCode = 403;
       activity.Response.Data = {
         ErrorText: "invalid X-Gitlab-Token"
@@ -23,16 +24,45 @@ module.exports = async (activity) => {
         let date = new Date(request.object_attributes.updated_at).toJSON();
         entity = {
           _type: "issue",
-          id: ""+request.object_attributes.id,
+          id: "" + request.object_attributes.id,
           title: request.object_attributes.title,
           description: request.object_attributes.description,
           date: date,
           link: request.object_attributes.url
-        }
+        };
 
-        // *todo* get email addresses, decide if issue is closed
-        collections.push({ name: "my", users: ["support@adenin.com"], date: date });
-        collections.push({ name: "open", users: ["ma@adenin.com", "support@adenin.com"], date: date });
+        if (request.object_attributes.state == "opened") {
+          api.initialize(activity);
+
+          let promises = [];
+          for (let i = 0; i < request.object_attributes.assignee_ids.length; i++) {
+            promises.push(api(`/users/${request.object_attributes.assignee_ids[i]}`));
+          }
+          // also get owner's info
+          promises.push(api(`/users/${request.object_attributes.author_id}`));
+
+          const responses = await Promise.all(promises);
+          for (let i = 0; i < responses.length; i++) {
+            if ($.isErrorResponse(activity, responses[i])) return;
+          }
+
+          let userMails = [];
+          for (let i = 0; i < responses.length-1; i++) {
+            if (responses[i].body.public_email != ""){
+             userMails.push(responses[i].body.public_email);
+            }
+          }
+
+          collections.push({ name: "my", users: userMails, date: date });
+
+          // push owner mail
+          let ownerMail = responses[promises.length-1].body.public_email;
+          if(ownerMail!="" && !userMails.includes(ownerMail)){
+            userMails.push(ownerMail);
+          }
+
+          collections.push({ name: "open", users: userMails, date: date });
+        }
         break;
 
       default:
@@ -40,9 +70,7 @@ module.exports = async (activity) => {
         break;
 
     }
-
     activity.Response.Data = { entity: entity, collections: collections };
-
   } catch (error) {
     $.handleError(activity, error);
   }
