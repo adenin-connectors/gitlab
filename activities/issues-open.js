8e9aa1d829a8d2edcca578c32d73d30788410bcc
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-06-05 10:46:40 +0200

    New activity issues-open.

--- activities/issues-open.js
+++ activities/issues-open.js
@@ -14,14 +14,14 @@ module.exports = async function (activity) {
 
     var dateRange = $.dateRange(activity, "today");
     var pagination = $.pagination(activity);
-    const response = await api(`/issues?state=all&scope=all&page=${pagination.page}&per_page=${pagination.pageSize}` +
+    const response = await api(`/issues?state=opened&scope=all&page=${pagination.page}&per_page=${pagination.pageSize}` +
       `&created_after=${dateRange.startDate}&created_before=${dateRange.endDate}`);
 
     if ($.isErrorResponse(activity, response)) return;
 
     activity.Response.Data.items = api.convertIssues(response.body);
     let value = activity.Response.Data.items.items.length;
-    activity.Response.Data.title = T(activity, 'All Issues');
+    activity.Response.Data.title = T(activity, 'Open Issues');
     activity.Response.Data.link = openIssuesUrl;
     activity.Response.Data.linkLabel = T(activity, 'All Issues');
     activity.Response.Data.actionable = value > 0;
@@ -29,10 +29,10 @@ module.exports = async function (activity) {
     if (value > 0) {
       activity.Response.Data.value = value;
       activity.Response.Data.color = 'blue';
-      activity.Response.Data.description = value > 1 ? T(activity, "There are {0} issues on Gitlab.", value)
-        : T(activity, "There is 1 issue on Gitlab.");
+      activity.Response.Data.description = value > 1 ? T(activity, "There are {0} open issues on Gitlab.", value)
+        : T(activity, "There is 1 open issue on Gitlab.");
     } else {
-      activity.Response.Data.description = T(activity, 'There are no issues on Gitlab');
+      activity.Response.Data.description = T(activity, 'There are no open issues on Gitlab');
     }
   } catch (error) {
     $.handleError(activity, error);
